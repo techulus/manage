5b10d27dbe84afb4a9d0ee5198ea0bd592eb00db
Arjun Komath <arjun@hey.com>
2023-07-02 09:17:26 +1000

    Clean up webhook setup

--- app/webhooks/auth/route.ts
+++ app/webhooks/auth/route.ts
@@ -1,20 +1,20 @@
 import { Webhook, WebhookRequiredHeaders } from "svix";
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-};
-
 const webhookSecret = process.env.AUTH_WEBHOOK_SECRET;
 
 export async function POST(request: Request) {
   const body = await request.json();
 
   const wh = new Webhook(webhookSecret as string);
+  const headers: WebhookRequiredHeaders = {
+    "svix-id": request.headers.get("svix-id") as string,
+    "svix-signature": request.headers.get("svix-signature") as string,
+    "svix-timestamp": request.headers.get("svix-timestamp") as string,
+  };
+
   let msg;
   try {
-    msg = wh.verify(body, request.headers as unknown as WebhookRequiredHeaders);
+    msg = wh.verify(body, headers);
   } catch (err) {
     new Response("Invalid signature", { status: 401 });
   }
