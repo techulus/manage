4c47d07a1863f9c9a9374c42eb67c3060abbb10f
Arjun Komath <arjun@hey.com>
2023-07-02 09:15:43 +1000

    Try webhook verify

--- app/webhooks/auth/route.ts
+++ app/webhooks/auth/route.ts
@@ -1,11 +1,25 @@
-export async function GET(request: Request) {
-  const body = await request.json();
-  console.log("GET /webhooks/auth", body);
-  return new Response("OK");
-}
+import { Webhook, WebhookRequiredHeaders } from "svix";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+const webhookSecret = process.env.AUTH_WEBHOOK_SECRET;
 
 export async function POST(request: Request) {
   const body = await request.json();
-  console.log("POST /webhooks/auth", body);
+
+  const wh = new Webhook(webhookSecret as string);
+  let msg;
+  try {
+    msg = wh.verify(body, request.headers as unknown as WebhookRequiredHeaders);
+  } catch (err) {
+    new Response("Invalid signature", { status: 401 });
+  }
+
+  console.log("POST /webhooks/auth", msg);
+
   return new Response("OK");
 }
