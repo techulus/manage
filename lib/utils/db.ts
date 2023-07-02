10be961345d38b91a4a89da37536028e4d67fce2
Arjun Komath <arjun@hey.com>
2023-07-02 19:29:17 +1000

    Fix iOS keyboard zoom

--- lib/utils/db.ts
+++ lib/utils/db.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 export const prisma = new PrismaClient({
-  log: ["query", "info", "warn", "error"],
+  // log: ["query", "info", "warn", "error"],
+  log: ["info", "warn", "error"],
 });
