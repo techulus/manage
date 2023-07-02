fdd9831cb59d7c8d5cbf408c06ea7eda1b273f85
Arjun Komath <arjun@hey.com>
2023-07-02 11:24:58 +1000

    Handle user payloads

--- app/webhooks/auth/route.ts
+++ app/webhooks/auth/route.ts
@@ -1,6 +1,19 @@
+import { prisma } from "@/lib/utils/db";
 import { Webhook, WebhookRequiredHeaders } from "svix";
 
 const webhookSecret = process.env.AUTH_WEBHOOK_SECRET;
+
+enum MessageTypes {
+  "user.created" = "user.created",
+  "user.updated" = "user.updated",
+  "user.deleted" = "user.deleted",
+}
+
+type Message = {
+  object: string;
+  type: MessageTypes;
+  data: any;
+};
 
 export async function POST(request: Request) {
   const wh = new Webhook(webhookSecret as string);
@@ -12,10 +25,42 @@ export async function POST(request: Request) {
 
   const body = await request.text();
 
-  let msg;
+  let msg: Message;
   try {
-    msg = wh.verify(body, headers);
-    console.log("POST /webhooks/auth Message:", msg);
+    msg = wh.verify(body, headers) as unknown as Message;
+    // console.log("POST /webhooks/auth Message:", msg);
+
+    const data = msg.data;
+
+    switch (msg.type) {
+      case "user.created":
+        await prisma.user.create({
+          data: {
+            id: data.id,
+            email: data.email_addresses[0].email_address,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            rawData: data,
+          },
+        });
+        break;
+      case "user.updated":
+        await prisma.user.update({
+          where: {
+            id: data.id,
+          },
+          data: {
+            email: data.email_addresses[0].email_address,
+            first_name: data.first_name,
+            last_name: data.last_name,
+            rawData: data,
+          },
+        });
+        break;
+      default:
+        throw new Error("Invalid message type");
+    }
+
     return new Response("OK");
   } catch (err) {
     console.log("POST /webhooks/auth Error:", err);
