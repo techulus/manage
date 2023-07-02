627a28d34229412a43e751a3c76898b71ef4160c
Arjun Komath <arjun@hey.com>
2023-07-02 11:11:01 +1000

    fix webhook

--- app/webhooks/auth/route.ts
+++ app/webhooks/auth/route.ts
@@ -3,14 +3,14 @@ import { Webhook, WebhookRequiredHeaders } from "svix";
 const webhookSecret = process.env.AUTH_WEBHOOK_SECRET;
 
 export async function POST(request: Request) {
-  const body = await request.json();
-
   const wh = new Webhook(webhookSecret as string);
   const headers: WebhookRequiredHeaders = {
     "svix-id": request.headers.get("svix-id") as string,
     "svix-signature": request.headers.get("svix-signature") as string,
     "svix-timestamp": request.headers.get("svix-timestamp") as string,
   };
+
+  const body = await request.text();
 
   let msg;
   try {
