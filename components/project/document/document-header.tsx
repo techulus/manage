74e4e0121d882044a6e05c2c2b5cfc1fe8439ac2
Arjun Komath <arjun@hey.com>
2023-07-09 19:05:28 +1000

    Fix link overlay

--- components/project/document/document-header.tsx
+++ components/project/document/document-header.tsx
@@ -3,13 +3,13 @@ import Link from "next/link";
 
 export const DocumentHeader = ({ document }: { document: Document }) => {
   return (
-    <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 dark:bg-gray-900 p-6">
+    <div className="relative flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6 dark:bg-gray-900">
       <Link
         href={`/console/projects/${document.projectId}/documents`}
         className="text-sm font-medium"
       >
         <span className="absolute inset-0" aria-hidden="true" />
-        <div className="font-medium text-xl leading-6">{document.name}</div>
+        <div className="text-xl font-medium leading-6">{document.name}</div>
       </Link>
     </div>
   );
