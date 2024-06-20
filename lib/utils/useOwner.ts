89a0ffe09ce012cc4df039a51a46a85d597f7130
Arjun Komath <arjun@hey.com>
2024-06-20 23:12:27 +1000

    chore: Add function to retrieve all users except the current user

--- lib/utils/useOwner.ts
+++ lib/utils/useOwner.ts
@@ -1,4 +1,6 @@
+import { User } from "@/drizzle/types";
 import { auth } from "@clerk/nextjs/server";
+import { database } from "./useDatabase";
 
 type Result = {
   ownerId: string;
@@ -9,4 +11,10 @@ type Result = {
 export function getOwner(): Result {
   const { userId, orgId } = auth();
   return { ownerId: orgId ?? userId ?? "", userId, orgId } as Result;
+}
+
+export async function allUser(): Promise<User[]> {
+  const { userId } = auth();
+  const users: User[] = (await database().query.user.findMany()) ?? [];
+  return users.filter((user) => user.id !== userId);
 }
