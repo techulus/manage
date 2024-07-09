ee2b12890f1420aa4bea3881aeaa7a2ca726e069
Arjun Komath <arjun@hey.com>
2024-07-09 20:16:59 +1000

    Fix trigger job

--- jobs/chores/handle-dormancy.ts
+++ jobs/chores/handle-dormancy.ts
@@ -11,15 +11,20 @@ client.defineJob({
   trigger: cronTrigger({
     cron: "0 9 * * *",
   }),
-  run: async (_, io, ctx) => {
+  run: async (_, io, __) => {
     const admin = opsDb();
 
-    const dormancyDate = new Date(Date.now() - 60 * 24 * 60 * 60 * 1000);
-
-    const users = await admin
-      .select()
-      .from(_user)
-      .where(lt(_user.lastActiveAt, dormancyDate));
+    const users = await io.runTask(
+      "fetch-users",
+      async () => {
+        const dormancyDate = new Date(Date.now() - 60 * 24 * 60 * 60 * 1000);
+        return await admin
+          .select()
+          .from(_user)
+          .where(lt(_user.lastActiveAt, dormancyDate));
+      },
+      { name: "Fetch users to deactivate" }
+    );
 
     await io.logger.info(`Found ${users.length} users to deactivate`);
   },
