ea6ceda9f21c9a53b30cf82d0b5027c214ac3420
Arjun Komath <arjun@hey.com>
2024-07-27 23:12:54 +1000

    Update trigger to v3

--- app/(api)/api/trigger/route.ts
+++
@@ -1,9 +0,0 @@
-import { client } from "@/trigger";
-import { createAppRoute } from "@trigger.dev/nextjs";
-
-import "@/jobs";
-
-//this route is used to send and receive data with Trigger.dev
-export const { POST, dynamic } = createAppRoute(client);
-
-export const maxDuration = 300;
--- jobs/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./chores/handle-dormancy";
