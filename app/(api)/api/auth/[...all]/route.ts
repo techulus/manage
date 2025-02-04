6d97306956f8f66ba2b7c5d8e8cd8de1db9e2d7e
Arjun Komath <arjunkomath@gmail.com>
2025-02-04 08:07:25 +0000

    Refactor auth handler

--- app/(api)/api/auth/[...all]/route.ts
+++ app/(api)/api/auth/[...all]/route.ts
@@ -1,4 +1,6 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
-export const { GET, POST } = toNextJsHandler(auth().handler);
+export const { GET, POST } = toNextJsHandler((request) =>
+	auth().handler(request),
+);
