bb1616ced10ca9ae7fe833289012d390961bb85b
Arjun Komath <arjun@hey.com>
2025-03-08 18:09:28 +1100

    Add broadcast URL configuration

--- lib/utils/turbowire.ts
+++ lib/utils/turbowire.ts
@@ -12,6 +12,8 @@ export async function broadcastEvent(
 	actor: string,
 	message: Record<string, string | number> | null = null,
 ) {
-	const turbowire = new TurboWireHub(process.env.TURBOWIRE_DOMAIN!);
+	const turbowire = new TurboWireHub(process.env.TURBOWIRE_DOMAIN!, {
+		broadcastUrl: process.env.TURBOWIRE_BROADCAST_URL,
+	});
 	await turbowire.broadcast(`${room}/${actor}`, JSON.stringify(message));
 }
